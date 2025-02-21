fc33ed99daf882bdd69ce874647e2aa26c156487
seulah <ssseulahhh@gmail.com>
2025-02-22 03:59:01 +0900

    chore: test code 작성을 위한 설정
    - application-test.yml 파일을 생성하여 "test" profile 에 국한한 환경 설정
    - test code에 사용할 H2 DB 설정
    - schema.sql을 만들어 테스트 시 테이블을 생성하게끔 설정

---
+++ src/test/java/com/project/fintech/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class DemoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
