c97c0f29d004a37298b25594c41592754e659372
seulah <ssseulahhh@gmail.com>
2025-02-22 03:55:16 +0900

    chore: Swagger 사용 환경 설정
    - Swagger 사용을 위한 config 파일 추가
    - Swagger 의존성 추가

---
+++ src/main/java/com/project/fintech/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.project.fintech.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "💳 Fintech Service",  // Swagger 제목
+        version = "1.0",  // API 버전
+        description = "fintech service API docs." // 설명
+    ),
+    servers = {
+        @Server(url = "http://localhost:8080", description = "로컬 서버"),
+    }
+)
+public class SwaggerConfig {
+
+}
