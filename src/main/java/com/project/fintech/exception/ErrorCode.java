ab8e8ef4861cc40b00f260d11c75a35e65e96c0a
seulah <ssseulahhh@gmail.com>
2025-02-28 23:59:56 +0900

    feat: JWT 인증 관련 ErrorCode 및 client에 반환할 Message 추가
    - JWT token verification 시 JWT Exception들에 대해 CustomException으로 처리 후 만료된 token, 유효하지 않은 token에 대한 ErorrCode 추가

--- src/main/java/com/project/fintech/exception/ErrorCode.java
+++ src/main/java/com/project/fintech/exception/ErrorCode.java
@@ -10,8 +10,11 @@ public enum ErrorCode {
     NOT_FOUND_USER("사용자를 찾을 수 없습니다.","AUTH-001", HttpStatus.BAD_REQUEST),
     ALREADY_EXIST_EMAIL("이미 가입된 이메일 입니다.", "AUTH-002", HttpStatus.BAD_REQUEST),
     EMAIL_NOT_VERIFIED("이메일 인증이 완료되지 않았습니다.", "AUTH-003", HttpStatus.BAD_REQUEST),
-    NOT_VALID_OTP_CODE("OTP 인증에 실패 하였습니다.", "AUTH-004", HttpStatus.BAD_REQUEST),
-    OTP_NOT_REGISTERED("OTP 인증을 위한 등록을 먼저 진행해주세요.", "AUTH-005", HttpStatus.BAD_REQUEST)
+    INVALID_OTP_CODE("OTP 인증에 실패 하였습니다.", "AUTH-004", HttpStatus.BAD_REQUEST),
+    OTP_NOT_REGISTERED("OTP 인증을 위한 등록을 먼저 진행해주세요.", "AUTH-005", HttpStatus.BAD_REQUEST),
+    INVALID_TOKEN("토큰이 유효하지 않습니다.", "AUTH-006", HttpStatus.BAD_REQUEST),
+    TOKEN_EXPIRED("토큰이 만료 되었습니다.", "AUTH-007", HttpStatus.BAD_REQUEST),
+    TOKEN_NOT_PROVIDED("토큰이 존재하지 않습니다.", "AUTH-008", HttpStatus.BAD_REQUEST),
     ;
 
     private final String detail;
--- src/main/java/com/project/fintech/model/type/Message.java
+++ src/main/java/com/project/fintech/model/type/Message.java
@@ -9,6 +9,8 @@ public enum Message {
     COMPLETE_VERIFY_EMAIL("이메일 인증이 완료"),
     COMPLETE_REGISTERED_OTP("OTP 등록이 완료"),
     COMPLETE_VERIFY_OTP("OTP 인증이 완료"),
+
+    COMPLETE_ISSUE_TOKEN("토큰 발급 완료")
     ;
     private final String message;
 
