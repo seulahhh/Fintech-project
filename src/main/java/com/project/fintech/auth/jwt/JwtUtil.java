91b7500b7bbc0ffae3835ae0d16d003474bc19e1
seulah <ssseulahhh@gmail.com>
2025-03-03 00:12:34 +0900

    feat: JWT Token 생성 및 검증하는 Util 클래스 추가
    - AccessToken, RefreshToken 생성 로직 구현
    - 토큰을 받아 검증하는 로직 구현
    - 사용자의 토큰으로 email값을 가져오는 로직 구현

---
+++ src/main/java/com/project/fintech/auth/jwt/JwtUtil.java
@@ -0,0 +1,100 @@
+package com.project.fintech.auth.jwt;
+
+import com.project.fintech.exception.CustomException;
+import com.project.fintech.exception.ErrorCode;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class JwtUtil {
+
+    private final StringRedisTemplate stringRedisTemplate;
+    private final String TOKEN_ISSUER = "Fintech_Service";
+    SecretKey key = Jwts.SIG.HS256.key().build();
+    @Value("${redis.key.prefix.disabled-token}")
+    private String DISABLED_TOKEN_PREFIX;
+
+    /**
+     * JWT Token basic builder 생성
+     *
+     * @param email
+     * @return JwtBuilder
+     */
+    public JwtBuilder tokenBuilder(String email) {
+        return Jwts.builder().issuedAt(new Date(System.currentTimeMillis())).issuer(TOKEN_ISSUER)
+            .audience().add(email).and().subject(email).signWith(key);
+    }
+
+    /**
+     * Access Token 생성
+     *
+     * @param email
+     * @return Access Token
+     */
+    public String generateAccessToken(String email) {
+        return tokenBuilder(email).expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+            .compact();
+    }
+
+    /**
+     * Refresh Token 생성
+     *
+     * @param email
+     * @return Refesh Token
+     */
+    public String generateRefreshToken(String email) {
+        return tokenBuilder(email).compact();
+    }
+
+    /**
+     * token으로부터 expiration 추출
+     *
+     * @param token
+     * @return Date expiration
+     */
+    public Date getTokenExpiration(String token) {
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload()
+            .getExpiration();
+    }
+
+    /**
+     * JWT Token으로부터 email 추출
+     *
+     * @param token
+     * @return email
+     */
+    public String getEmailFromToken(String token) {
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload()
+            .getSubject();
+    }
+
+    /**
+     * JWT Token 유효성 검증
+     *
+     * @param token
+     * @return true / false
+     */
+    public void verifyToken(String token) throws JwtException {
+        if (stringRedisTemplate.hasKey(DISABLED_TOKEN_PREFIX + token)) {
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+        try {
+            Jwts.parser().verifyWith(key).build().parse(token);
+        } catch (ExpiredJwtException e) {
+            throw new CustomException(ErrorCode.TOKEN_EXPIRED);
+        } catch (JwtException e) {
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+    }
+}
