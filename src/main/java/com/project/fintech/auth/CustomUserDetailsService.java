ae8f645c5e0f9ea88823566874c81b79c03851e3
seulah <ssseulahhh@gmail.com>
2025-03-03 00:55:20 +0900

    refactor: 발생 가능한 예외 Type 변경
    - (RuntimeException -> CustomException)

--- src/main/java/com/project/fintech/auth/CustomUserDetailsService.java
+++ src/main/java/com/project/fintech/auth/CustomUserDetailsService.java
@@ -1,12 +1,16 @@
 package com.project.fintech.auth;
 
+import com.project.fintech.exception.CustomException;
+import com.project.fintech.exception.ErrorCode;
 import com.project.fintech.persistence.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
@@ -15,7 +19,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
-        return userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("")); // todo
+        return userRepository.findByEmail(email).orElseThrow(() -> new CustomException(
+            ErrorCode.NOT_FOUND_USER)); // todo
     }
 }
