9979a42fd481e45d933c3fa52ee98c3e0d9b7f2a
seulah <ssseulahhh@gmail.com>
2025-03-07 01:20:40 +0900

    fix: AuthenticationException 처리 방식 수정
    AuthenticationException은 필터 체인 외부에서 호출 되기 때문에 ExceptionHandler로 보내 주지 못하는 문제 발생 -> 직접 AuthenticationEntryPoint에서 처리하도록 수정

--- src/main/java/com/project/fintech/auth/springsecurity/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/project/fintech/auth/springsecurity/CustomAuthenticationEntryPoint.java
@@ -1,25 +1,36 @@
 package com.project.fintech.auth.springsecurity;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.fintech.exception.CustomException;
+import com.project.fintech.exception.CustomException.CustomExceptionResponse;
+import com.project.fintech.exception.ErrorCode;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private final HandlerExceptionResolver resolver;
-    public CustomAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
-        this.resolver = resolver;
-    }
+    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json;charset=UTF-8");
 
-        resolver.resolveException(request,response,null, authException);
+        CustomExceptionResponse customExceptionResponse = CustomException.toResponse(
+            new CustomException(ErrorCode.LOGIN_REQUEST_FAIL));
+        String customExceptionResponseJson = objectMapper.writeValueAsString(customExceptionResponse);
+        response.getWriter().write(customExceptionResponseJson);
+        response.getWriter().flush();
     }
 }
