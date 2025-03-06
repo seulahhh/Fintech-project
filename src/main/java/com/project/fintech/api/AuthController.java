2941b68007badf70e0ff33ef7228bca73dc9645a
seulah <ssseulahhh@gmail.com>
2025-03-07 00:26:34 +0900

    feat: OTP Code 검증 API 추가

--- src/main/java/com/project/fintech/api/AuthController.java
+++ src/main/java/com/project/fintech/api/AuthController.java
@@ -2,8 +2,10 @@ package com.project.fintech.api;
 
 import com.project.fintech.application.AuthApplication;
 import com.project.fintech.model.dto.IssueTokenRequestDto;
+import com.project.fintech.model.dto.OtpVerificationDto;
 import com.project.fintech.model.dto.ResponseDto;
 import com.project.fintech.model.dto.TokenPairDto;
+import com.project.fintech.model.dto.UserEmailDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,16 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthApplication authApplication;
-    // ProvisioningUrl(=secretKey) 재발급
-//    @PostMapping("/auth/otp/reset")
-//    public ResponseEntity<?> resetSecretKey(@RequestParam String email) {
-        // SecretKey를 재생성
-        // Provisiong URL을 생성
-        // 등록 가능한 경로 만들기
-        // 등록 했다면 또 OTP 인증을 수행하기
-//        return null;
-//    } --> OTP 인증 및 재발급과 관련된 컨트롤러는 로그인, 로그아웃과 관련이 없어
-//    잠시 주석 처리 해두었습니다. 해당 부분 구현 후 해제하겠습니다
 
     /**
      * OTP secret key를 재발급하고 Provision URL 반환
@@ -35,10 +27,28 @@ public class AuthController {
     public ResponseEntity<ResponseDto<String>> resetSecretKey(@RequestBody UserEmailDto userEmailDto) {
         return ResponseEntity.ok(authApplication.issueNewOtpSecretAndSendUrl(userEmailDto));
     }
+
+    /**
+     * Access Token과 Refresh Token을 발급
+     *
+     * @param issueTokenRequestDto
+     * @return
+     */
     @PostMapping("/auth/jwt/issue")
     public ResponseEntity<ResponseDto<TokenPairDto>> issueTokens(
         @RequestBody IssueTokenRequestDto issueTokenRequestDto) {
         return ResponseEntity.ok(
             authApplication.issueNewAccessTokenByRefreshToken(issueTokenRequestDto));
+    }
+
+    /**
+     * OTP 코드를 검증
+     * @param otpVerificationDto
+     * @param session
+     * @return
+     */
+    @PostMapping("/auth/otp/verify")
+    public ResponseEntity<ResponseDto<String>> verifyOtpCode(@RequestBody OtpVerificationDto otpVerificationDto) {
+        return ResponseEntity.ok(authApplication.executeOtpVerification(otpVerificationDto));
     }
 }
