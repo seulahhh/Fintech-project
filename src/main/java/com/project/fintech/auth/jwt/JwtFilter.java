30ccc1dd285e4004fe995daea944dd3971ba1bdf
seulah <ssseulahhh@gmail.com>
2025-03-07 15:57:25 +0900

    fix: JWT Filter를 bean등록해서 생성자 주입 -> 필요한 filterchain에서 생성해서 사용하도록 수정
    - Component로 등록하면 security filter chain에서 원하는 지점에 .addfilter()로 등록해 준다고 하더라도 전역 Filter로 등록되어 원하는 경로 외에도 작동하는 문제가 발생하는 이슈로 인해 수정

--- src/main/java/com/project/fintech/auth/jwt/JwtFilter.java
+++ src/main/java/com/project/fintech/auth/jwt/JwtFilter.java
@@ -9,12 +9,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
 public class JwtFilter extends OncePerRequestFilter {
 
     private final AuthApplication authApplication;
